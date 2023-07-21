954fb7eb339216533e2e68c8ff00a4fa23abd2a7
mitsukiraikiri <mitsukiraikiri@gmail.com>
2023-07-21 19:24:12 +0800

    update

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const TesseractConfig = {
 
 // Inisialisasi Express.js
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/node_ocr', (req, res) => {
     res.send('Hello, World!');
   });
 // Route untuk mengenali teks dari URL gambar
-app.post('/recognize', async (req, res) => {
+app.post('/node_ocr/recognize', async (req, res) => {
   const imageUrl = req.body.imageUrl;
 
   if (!imageUrl) {
